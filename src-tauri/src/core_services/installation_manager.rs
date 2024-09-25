2e31d45f4624c9a4dbb5544e891a0bb226f4acc0
morganhiggin2 <morganhigginbotham2@gmail.com>
2024-09-24 19:39:35 -0700

    virtual directory development complete, pending testing

--- src-tauri/src/core_services/installation_manager.rs
+++ src-tauri/src/core_services/installation_manager.rs
@@ -1,17 +1,16 @@
 use std::{
     env::home_dir,
     fs::{self, create_dir_all, read_dir},
+    io::Write,
     path::PathBuf,
 };
 
 use super::concurrent_processes::get_runables_directory;
 
 // The files needed for the base python runables
-const snippet_creator_file_contents: &str = include_str!("../../runables/snippet_creator.py");
-const snippet_runner_file_contents: &str = include_str!("../../runables/snippet_runner.py");
-
 /// Responsible for installing the files upon launch in the right location on the computer if not already there
-/// The files to install will be bundled with the launcher
+/// The files to install will be bundled with the launcherconst SNIPPET_CREATOR_FILE_CONTENTS: &str = include_str!("../../runables/snippet_creator.py");
+const SNIPPET_RUNNER_FILE_CONTENTS: &str = include_str!("../../runables/snippet_runner.py");
 
 // structs for the visual directory
 struct VirtualFolder {
@@ -26,29 +25,37 @@ struct VirtualFile {
 }
 
 // create static virtual file directory
-const virutal_directory: VirtualFolder = VirtualFolder {
+const VIRTUAL_DIRECTORY: VirtualFolder = VirtualFolder {
     name: &"runables",
     files: &[
         VirtualFile {
             name: "snippet_runner.py",
-            contents: snippet_runner_file_contents,
+            contents: SNIPPET_RUNNER_FILE_CONTENTS,
         },
         VirtualFile {
             name: "snippet_creator.py",
-            contents: snippet_creator_file_contents,
+            contents: SNIPPET_CREATOR_FILE_CONTENTS,
         },
     ],
-    folders: &[],
+    folders: &[VirtualFolder {
+        name: "snippets",
+        files: &[],
+        folders: &[VirtualFolder {
+            name: "root",
+            files: &[],
+            folders: &[],
+        }],
+    }],
 };
 
-pub fn install_runables(app: &mut tauri::App) {
+pub fn install_runables() {
     let install_path = get_runables_directory();
 
     // create base directory if not exists
     create_dir_all(install_path.to_owned()).unwrap();
 
     // create visual direction in install location
-    put_runable_crawler(source_path, install_path).unwrap();
+    install_runable_crawer(&VIRTUAL_DIRECTORY, install_path).unwrap();
 }
 
 /// helper method to install files from the virual directory
@@ -75,7 +82,7 @@ fn install_runable_crawer(folder: &VirtualFolder, install_path: PathBuf) -> Resu
         let child_install_path = install_path.to_owned().join(child_file.name.to_owned());
 
         // create file
-        let mut file: fs::File = match fs::File::create(child_install_path) {
+        let mut file = match fs::File::create(child_install_path.to_owned()) {
             Ok(some) => some,
             Err(e) => {
                 return Err(format!(
@@ -87,7 +94,7 @@ fn install_runable_crawer(folder: &VirtualFolder, install_path: PathBuf) -> Resu
         };
 
         // write contents
-        match file.write_all(child_file.contents) {
+        match file.write_all(child_file.contents.as_bytes()) {
             Ok(_) => (),
             Err(e) => {
                 return Err(format!(
@@ -109,86 +116,4 @@ fn install_runable_crawer(folder: &VirtualFolder, install_path: PathBuf) -> Resu
     }
 
     return Ok(());
-}
-
-/// helper method to install files from source directory to installation directory if they do not already exist
-///
-/// # Arguments:
-///
-/// * 'source_path' - source path
-/// * 'install_path' - install path
-/// * 'relative_path' - relative path
-fn put_runable_crawler(source_path: PathBuf, install_path: PathBuf) -> Result<(), String> {
-    // if the install path is a directory and the source path does not exist
-    if source_path.is_dir() {
-        // if it does not exist already
-        if !install_path.exists() {
-            // create the directory if not exist
-            match fs::create_dir(install_path.to_owned()) {
-                Ok(_) => (),
-                Err(e) => {
-                    return Err(format!(
-                        "Could not create directory at location {}: {}",
-                        install_path.to_owned().to_string_lossy(),
-                        e.to_string()
-                    ));
-                }
-            };
-        }
-
-        // walk child directories
-        let directory_entries = match read_dir(source_path.to_owned()) {
-            Ok(some) => some,
-            Err(e) => {
-                return Err(format!(
-                    "Could not read directory entries in path {}: {}",
-                    source_path.to_string_lossy(),
-                    e.to_string()
-                ));
-            }
-        };
-
-        for dir_entry_result in directory_entries {
-            let dir_entry = match dir_entry_result {
-                Ok(some) => some,
-                Err(e) => {
-                    return Err(format!(
-                        "Could not get directory entry of parent path {}: {}",
-                        install_path.to_string_lossy(),
-                        e.to_string()
-                    ));
-                }
-            };
-
-            // get path from directory entry
-            let child_source_path = dir_entry.path();
-
-            // get install path
-            let child_install_path = install_path
-                .to_owned()
-                .join(child_source_path.file_name().unwrap());
-
-            // make child call
-            put_runable_crawler(child_source_path, child_install_path)?;
-        }
-    }
-    // if the source path is a file and the install path does not exist
-    else if !source_path.is_dir() && !install_path.exists() {
-        // copy the file
-        //create the directory if not exist
-        match fs::copy(source_path.to_owned(), install_path.to_owned()) {
-            Ok(_) => (),
-            Err(e) => {
-                return Err(format!(
-                    "Could not create directory at location {}: {}",
-                    install_path.to_string_lossy(),
-                    e.to_string()
-                ));
-            }
-        };
-    }
-
-    return Ok(());
-
-    // if the install path is a file, install file if it does not exist
 }
--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -8,31 +8,53 @@ use std::ops::DerefMut;
 use core_services::installation_manager::install_runables;
 
 //use snippet_python_module::python_module::call_init_2;
-use crate::state_management::{SharedApplicationState, ApplicationState};
-use crate::tauri_services::directory_tauri_service::{get_snippet_directory_details, spawn_initialize_snippet_directory};
-use crate::tauri_services::snippet_tauri_service::{new_snippet, validate_pipeline_connection, new_pipeline, check_pipeline_connector_capacity_full, get_id, get_pipeline_connector_uuids_from_pipeline, delete_pipeline, get_snippet_pipelines, get_pipeline_connector_uuids_from_snippet, delete_snippet, update_snippet_parameter_value, spawn_run_snippets};
-use crate::tauri_services::window_session_tauri_service::{new_window_session};
+use crate::state_management::{ApplicationState, SharedApplicationState};
+use crate::tauri_services::directory_tauri_service::{
+    get_snippet_directory_details, spawn_initialize_snippet_directory,
+};
+use crate::tauri_services::snippet_tauri_service::{
+    check_pipeline_connector_capacity_full, delete_pipeline, delete_snippet, get_id,
+    get_pipeline_connector_uuids_from_pipeline, get_pipeline_connector_uuids_from_snippet,
+    get_snippet_pipelines, new_pipeline, new_snippet, spawn_run_snippets,
+    update_snippet_parameter_value, validate_pipeline_connection,
+};
+use crate::tauri_services::window_session_tauri_service::new_window_session;
 
-pub mod state_management;
-pub mod utils;
 pub mod core_components;
-pub mod tauri_services;
 pub mod core_services;
 pub mod python_libraries;
+pub mod state_management;
+pub mod tauri_services;
+pub mod utils;
 
 fn main() {
     //create application state
     let application_state_guard = SharedApplicationState::default();
 
-    tauri::Builder::default()
-        .setup(|app| {
-            // initialize installation files 
-            install_runables(app);
+    // install necessary python runable files
+    install_runables();
 
-            return Ok(());
-        })
+    tauri::Builder::default()
+        .setup(|_app| return Ok(()))
         .manage(application_state_guard)
-        .invoke_handler(tauri::generate_handler![logln, new_window_session, new_snippet, validate_pipeline_connection, new_pipeline, check_pipeline_connector_capacity_full, get_id, get_pipeline_connector_uuids_from_pipeline, delete_pipeline, get_snippet_pipelines, get_pipeline_connector_uuids_from_snippet, delete_snippet, spawn_initialize_snippet_directory, get_snippet_directory_details, update_snippet_parameter_value, spawn_run_snippets])
+        .invoke_handler(tauri::generate_handler![
+            logln,
+            new_window_session,
+            new_snippet,
+            validate_pipeline_connection,
+            new_pipeline,
+            check_pipeline_connector_capacity_full,
+            get_id,
+            get_pipeline_connector_uuids_from_pipeline,
+            delete_pipeline,
+            get_snippet_pipelines,
+            get_pipeline_connector_uuids_from_snippet,
+            delete_snippet,
+            spawn_initialize_snippet_directory,
+            get_snippet_directory_details,
+            update_snippet_parameter_value,
+            spawn_run_snippets
+        ])
         .run(tauri::generate_context!())
         .expect("error while starting tauri application");
 }
@@ -42,5 +64,5 @@ fn logln(text: &str) {
     println!("{text}");
 }
 
-//IMPORTAINT 
+//IMPORTAINT
 //installed libpython3.10-dev
